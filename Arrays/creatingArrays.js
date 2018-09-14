d75fff0bbeae94435e64a07007fd5f6e24091d5b
Justine Philip <philip.obosi@eng.uniben.edu>
2018-09-14 22:24:54 +0100

    basic setup

---
+++ Arrays/creatingArrays.js
@@ -0,0 +1,15 @@
+// Declaring Arrays
+
+// Method 1. Using the [] operator
+let devs = [];
+
+// Method 2. [] operator with elements
+let devs = ['jens.codes', 'wesbos', 'dhanishgajjar', 'worldclassdev'];
+
+// Method 3. Using the Array constructor
+let devs = new Array();
+
+// Method 4. Array constructor with elements
+let devs = new Array(1,2,3,4,5);
+
+// Creating an Array of length n
